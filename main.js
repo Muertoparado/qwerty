dfa779d7759070ceebee2c7e391589f778fdb4f7
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-07 09:20:41 -0500

    primera cadena

---
+++ main.js
@@ -0,0 +1,3 @@
+cadena="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum praesentium blanditiis saepe tempora ipsam aperiam id doloremque laudantium quae voluptatem accusantium earum"
+
+console.log(cadena)
