1174f1b9e9a53427c2742e62834059923da0efe7
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-08 06:42:55 -0500

    feat: metodo replace

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 cadena="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum praesentium blanditiis saepe tempora ipsam aperiam id doloremque laudantium quae voluptatem accusantium earum"
 
-console.log(cadena.includes('q'));/* true */
-console.log(cadena.includes('z')); /* false */
+console.log(cadena.replace('Lorem', 'Hola mundo'));
