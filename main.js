acec41668e735fb9d53c649fd414a844ff725607
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-08 06:47:01 -0500

    feat: metodo slice

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-cadena="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum praesentium blanditiis saepe tempora ipsam aperiam id doloremque laudantium quae voluptatem accusantium earum"
+var cadena="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum praesentium blanditiis saepe tempora ipsam aperiam id doloremque laudantium quae voluptatem accusantium earum"
 
-console.log(cadena.replace('Lorem', 'Hola mundo'));
+var copiacadena= cadena.slice(10,20);
+console.log(cadena);
+console.log(copiacadena);
+
