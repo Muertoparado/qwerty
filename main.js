8b1115a37ae471cfeb310dd922b362a6665a8a21
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-08 09:09:46 -0500

    feat: :rocket: obtener caracter de posicion exacta

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 var cadena="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum praesentium blanditiis saepe tempora ipsam aperiam id doloremque laudantium quae voluptatem accusantium earum"
 
-var copiacadena= cadena.slice(10,20);
-console.log(cadena);
-console.log(copiacadena);
-
+console.log(cadena.charAt(2)); /* obtener caracter en la cadena */
