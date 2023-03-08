4aa8327252059a786d2a07e9de98f05bc5e47c59
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-08 06:39:20 -0500

    feat: metodo includes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 cadena="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum praesentium blanditiis saepe tempora ipsam aperiam id doloremque laudantium quae voluptatem accusantium earum"
 
-console.log(cadena)
+console.log(cadena.includes('q'));/* true */
+console.log(cadena.includes('z')); /* false */
