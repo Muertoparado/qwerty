7273f5a4bf9ab33e7bf070856d57fe7d1c3ea708
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-09 08:48:09 -0500

    feat: :rocket: asigancion estricta

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 x=3
 y=4
-/* asigancion multiplicacion  es igua la suma resta etc equivale y=y*x */
-console.log(`el dato aignado a x es eqivalente a : ${y*=x}`)
+
+if(x==y){
+    console.log("son iguales los numeros ")
+}
+/* asigancion estricta de igualdad  */
+console.log(`numeros distintos ${x} !== ${y}`);
