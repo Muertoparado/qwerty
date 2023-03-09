f76ab5f12e2e4682e35570e1296d544958a8b334
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-09 13:40:59 -0500

    feat: :tada: compuerta and

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 x=prompt("introducir x numero a evaluar")
 y=prompt("introducir y numero a evaluar")
+num=3
 
-/* operador residuo  */
-console.log(`operador residuo (%) ${x}%${y}: ${x%y}`);
+/* compuerta and */
+if(num >=x && num<=y){
+    console.log(` numero base ${num} compuerta and (&&) numeros ingresados mayores `);
+
+
+}
+console.log('los numeros no fueron mayores')
