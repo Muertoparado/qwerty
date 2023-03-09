e7d72997d0ff8b3d2d54aecd3cb5ccd8ca4bf5cc
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-09 09:18:09 -0500

    feat: :rocket: opoerador residuo

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 x=prompt("introducir x numero a evaluar")
 y=prompt("introducir y numero a evaluar")
 
-if(x>=y){
-    console.log("es mayor o igual ")
-}
-/* asigancion mayor o igual   */
-console.log(`no es mayor o igual ${x} a ${y}`);
+/* operador residuo  */
+console.log(`operador residuo (%) ${x}%${y}: ${x%y}`);
