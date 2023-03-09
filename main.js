a61e78f2fe9887615a0a263f0b971246b62e6cde
Muertoparado <nicolas.casanova20@gmail.com>
2023-03-09 07:40:38 -0500

    feat: :rocket: metodo trim

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-var cadena="Lorem m 1 sdsdsdsd"
+var cadena="                Lorem m 1 sdsdsdsd                "
 
-console.log(cadena.toUpperCase()); /* devulve la cadena con letras en mayusculas */
+console.log(cadena.trim()); /* eliminar espacios en la cadena*/
