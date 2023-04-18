4bafd219f1719fcd4cdadea57f5234e3096dd6a4
Muertoparado <nicolas.casanova20@gmail.com>
2023-04-18 09:29:54 -0500

    feat: :recycle: intento callback

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-x=prompt("introducir x numero a evaluar")
-y=prompt("introducir y numero a evaluar")
-num=3
+/* funcion callbacks() */
+/* fCallback=()=>{
+    console.log("ESTA LLAMANDO LA FUNCION CALLBACK ");
+};
+llamadoCallback=()=>{
+    console.log("funcion principal");
+    fCallback();
+} */
 
-/* compuerta and */
-if(num >=x && num<=y){
-    console.log(` numero base ${num} compuerta and (&&) numeros ingresados mayores `);
-
-
-}
-console.log('los numeros no fueron mayores')
+const fcall=(op,call)=>{
+    num1=Number(prompt("digite num 1"))
+    num2=Number(prompt("digite num 2"))
+    op=num1+num2
+    call({
+        
+    });
+    }
